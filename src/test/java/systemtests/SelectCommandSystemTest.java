68af67ce39cb3dc6cae5e3a11f30390f0977267c
lingyanhao <lingyanhao@live.com>
2019-02-17 21:49:02 +0800

    change tests to reflect accepting mixed case commands

--- src/test/java/systemtests/SelectCommandSystemTest.java
+++ src/test/java/systemtests/SelectCommandSystemTest.java
@@ -89,9 +89,6 @@ public class SelectCommandSystemTest extends AddressBookSystemTest {
         assertCommandFailure(SelectCommand.COMMAND_WORD + " 1 abc",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, SelectCommand.MESSAGE_USAGE));
 
-        /* Case: mixed case command word -> rejected */
-        assertCommandFailure("SeLeCt 1", MESSAGE_UNKNOWN_COMMAND);
-
         /* Case: select from empty address book -> rejected */
         deleteAllPersons();
         assertCommandFailure(SelectCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased(),
