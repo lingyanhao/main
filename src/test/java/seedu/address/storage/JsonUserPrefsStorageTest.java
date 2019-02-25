a26ca33c6b8678cf5560d63465ebba8656f5f0df
hiyingnn <r.ying98@gmail.com>
2019-02-25 14:17:42 +0800

    Edited Test Files

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -83,7 +83,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setRestaurantBookFilePath(Paths.get("addressbook.json"));
+        userPrefs.setRestaurantBookFilePath(Paths.get("data", "addressbook.json"));
         return userPrefs;
     }
 
