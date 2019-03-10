c4b4027e72c8e0fc55f2f270d84be5c8bfbf89f7
limli <limlirandom@gmail.com>
2019-03-10 20:30:52 +0800

    add header for cards in UI

--- src/main/java/seedu/address/ui/IngredientListPanel.java
+++ src/main/java/seedu/address/ui/IngredientListPanel.java
@@ -4,6 +4,8 @@ import java.util.function.Consumer;
 
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import seedu.address.model.ingredient.Ingredient;
 
@@ -12,10 +14,14 @@ import seedu.address.model.ingredient.Ingredient;
  */
 public class IngredientListPanel extends ItemListPanel<Ingredient> {
 
+    @FXML
+    private Label title;
+
     public IngredientListPanel(ObservableList<Ingredient> ingredientList,
                                ObservableValue<Ingredient> selectedIngredient,
                                Consumer<Ingredient> onSelectedIngredientChange) {
         super(ingredientList, selectedIngredient, onSelectedIngredientChange, listview -> new IngredientListViewCell());
+        title.setText("Ingredient");
     }
 }
 
