e9ad808b92bbb1fe71a542ba6489c5c633b80879
limli <limlirandom@gmail.com>
2019-03-15 21:54:22 +0800

    create window for bar chart

---
+++ src/main/java/seedu/address/ui/StatsWindow.java
@@ -0,0 +1,79 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import seedu.address.commons.core.LogsCenter;
+
+/**
+ * Controller for the stats page
+ */
+public class StatsWindow extends UiPart<Stage> {
+
+    private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
+    private static final String FXML = "StatsWindow.fxml";
+
+    @FXML
+    private StackPane mystackpane;
+
+    public StatsWindow(Stage root) {
+        super(FXML, root);
+
+        final String austria = "Austria";
+        final String brazil = "Brazil";
+        final String france = "France";
+        final String italy = "Italy";
+        final String usa = "USA";
+
+
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final BarChart<String,Number> bc = new BarChart<>(xAxis,yAxis);
+        bc.setTitle("Country Summary");
+        xAxis.setLabel("Country");
+        yAxis.setLabel("Value");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("2003");
+        series1.getData().add(new XYChart.Data(austria, 25601.34));
+        series1.getData().add(new XYChart.Data(brazil, 20148.82));
+        series1.getData().add(new XYChart.Data(france, 10000));
+        series1.getData().add(new XYChart.Data(italy, 35407.15));
+        series1.getData().add(new XYChart.Data(usa, 12000));
+
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("2004");
+        series2.getData().add(new XYChart.Data(austria, 57401.85));
+        series2.getData().add(new XYChart.Data(brazil, 41941.19));
+        series2.getData().add(new XYChart.Data(france, 45263.37));
+        series2.getData().add(new XYChart.Data(italy, 117320.16));
+        series2.getData().add(new XYChart.Data(usa, 14845.27));
+
+        XYChart.Series series3 = new XYChart.Series();
+        series3.setName("2005");
+        series3.getData().add(new XYChart.Data(austria, 45000.65));
+        series3.getData().add(new XYChart.Data(brazil, 44835.76));
+        series3.getData().add(new XYChart.Data(france, 18722.18));
+        series3.getData().add(new XYChart.Data(italy, 17557.31));
+        series3.getData().add(new XYChart.Data(usa, 92633.68));
+
+        bc.getData().addAll(series1, series2, series3);
+
+        mystackpane.getChildren().add(bc);
+    }
+
+    public StatsWindow() {
+        this(new Stage());
+    }
+
+    public void show() {
+        logger.fine("Showing help page about the application.");
+        getRoot().show();
+    }
+}
