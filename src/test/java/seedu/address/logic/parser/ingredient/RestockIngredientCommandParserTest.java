bee5426106026522be1bab683fc77e1e52b68666
hiyingnn <r.ying98@gmail.com>
2019-03-22 14:00:05 +0800

    Edited RestockIngredientCommandParserTest

--- src/test/java/seedu/address/logic/parser/ingredient/RestockIngredientCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ingredient/RestockIngredientCommandParserTest.java
@@ -2,11 +2,12 @@ package seedu.address.logic.parser.ingredient;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.INGREDIENT_QUANTITY_DESC_CHEESE;
-import static seedu.address.logic.commands.CommandTestUtil.INGREDIENT_VALID_QUANTITY_CHEESE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_INDEX_DESC;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INGREDIENT_QUANTITY;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_INGREDIENT;
+import static seedu.address.testutil.TypicalIngredients.TYPICAL_CONSUME_AMOUNT;
 
 import org.junit.Test;
 
@@ -32,10 +33,10 @@ public class RestockIngredientCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsRestockIngredientCommand() {
-
-        assertParseSuccess(parser, VALID_INDEX_DESC + INGREDIENT_QUANTITY_DESC_CHEESE,
+        assertParseSuccess(parser, VALID_INDEX_DESC + " "
+                        + PREFIX_INGREDIENT_QUANTITY + TYPICAL_CONSUME_AMOUNT,
                 new RestockIngredientCommand(INDEX_FIRST_INGREDIENT,
-                        new IngredientQuantity(Integer.parseInt(INGREDIENT_VALID_QUANTITY_CHEESE))));
+                        new IngredientQuantity(TYPICAL_CONSUME_AMOUNT)));
     }
 }
 
